2d3163eb6035bb7b56c6588fc3e1a701846b8c6c
Konstantin Lyang <konst613@mail.ru>
2023-08-01 10:40:28 +0700

    ChatManager done

--- JurChat.Server/Hubs/ChatHub.cs
+++ JurChat.Server/Hubs/ChatHub.cs
@@ -3,23 +3,49 @@ using Microsoft.AspNetCore.SignalR;
 
 namespace JurChat.Server.Hubs
 {
-    public class ChatHub : Hub
+    public class ChatHub : Hub<IChatHub>
     {
+        public ChatHub(ChatHubManager hubManager) => _chatHubManager = hubManager;
+        
         private readonly ChatHubManager _chatHubManager;
+        
+        private const string DefaultGroupName = "KPPGROUP";
 
-        public ChatHub(ChatHubManager hubManager)
-        {
-            _chatHubManager = hubManager;
-        }
+        #region Overrides
 
         public override async Task OnConnectedAsync()
         {
+            _chatHubManager.ConnectUser(Guid.NewGuid().ToString(), Context.ConnectionId);
+
+            await Groups.AddToGroupAsync(Context.ConnectionId, DefaultGroupName);
+            await UpdateUsersAsync();
             await base.OnConnectedAsync();
         }
 
         public override async Task OnDisconnectedAsync(Exception? exception)
         {
+            if(_chatHubManager.DisconnectUser(Context.ConnectionId)) await base.OnDisconnectedAsync(exception);
+
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, DefaultGroupName);
+            await UpdateUsersAsync();
             await base.OnDisconnectedAsync(exception);
         }
+
+        #endregion
+
+        public async Task UpdateUsersAsync()
+        {
+            var users = _chatHubManager.ConnectedUsers.Select(x => x.Id).ToList();
+            await Clients.All.UpdateUsersAsync(users);
+        }
+
+        public async Task SendPrivateMessageAsync(string senderId, string recipientId, byte[] message) =>
+            await Clients.All.SendPrivateMessageAsync(senderId, recipientId, message);
+    }
+
+    public interface IChatHub
+    {
+        public Task UpdateUsersAsync(IEnumerable<string> users);
+        public Task SendPrivateMessageAsync(string senderId, string recipientId, byte[] message);
     }
 }
--- JurChat.Server/Hubs/Infrastructure/ChatHubManager.cs
+++ JurChat.Server/Hubs/Infrastructure/ChatHubManager.cs
@@ -3,5 +3,61 @@
     public class ChatHubManager
     {
         public List<UserConnection> ConnectedUsers { get; set; } = new();
+
+        public bool ConnectUser(string userId, string connectionId)
+        {
+            try
+            {
+                var existsUser = GetConnectedUserById(userId);
+                if (existsUser != null)
+                {
+                    existsUser.AppendConnection(connectionId);
+                    return true;
+                }
+
+                UserConnection user = new(userId);
+                user.AppendConnection(connectionId);
+                ConnectedUsers.Add(user);
+                return true;
+            }
+            catch (Exception ex) { return false; }
+        }
+
+        public bool DisconnectUser(string connectionId)
+        {
+            try
+            {
+                var user = GetConnectedUserByConnectionId(connectionId);
+
+                if (user == null) return true;
+
+                if (user.Connections.Count() == 1)
+                {
+                    user.RemoveConnection(user.Connections.Last().ConnectionId);
+                    ConnectedUsers.Remove(user);
+                    return true;
+                }
+
+                if (!user.Connections.Any())
+                {
+                    ConnectedUsers.Remove(user);
+                    return true;
+                }
+
+                user.RemoveConnection(user.Connections.Last().ConnectionId);
+                
+                return true;
+            }
+            catch(Exception ex)
+            {
+                return false;
+            }
+        }
+
+        private UserConnection? GetConnectedUserById(string userId) =>
+            ConnectedUsers.FirstOrDefault(x => x.Id == userId);
+
+        private UserConnection? GetConnectedUserByConnectionId(string connectionId) =>
+            ConnectedUsers.FirstOrDefault(x => x.Connections.Select(c => c.ConnectionId).Contains(connectionId));
     }
 }
